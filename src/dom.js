cf3ebd39d411bf2a4956c08579496b4716aaef55
Ed Benedict Quia <edbenedictquia@gmail.com>
2024-03-02 19:13:56 +0800

    add sidebar

--- src/dom.js
+++ src/dom.js
@@ -1,5 +1,21 @@
 function sidebar() {
+    const div = document.createElement('div');
+    const addTaskBtn = document.createElement('button');
+    const showAllBtn = document.createElement('button');
+    const showTodayBtn = document.createElement('button');
+    const showUpcomingBtn = document.createElement('button');
+    const showCategoryBtn = document.createElement('button');
 
+    div.setAttribute('id', 'sidebar');
+    addTaskBtn.setAttribute('id', 'addTaskBtn');
+    showAllBtn.setAttribute('id', 'showAllBtn');
+    showTodayBtn.setAttribute('id', 'showTodayBtn');
+    showUpcomingBtn.setAttribute('id', 'showUpcomingBtn');
+    showCategoryBtn.setAttribute('id', 'showCategoryBtn');
+
+    div.append(addTaskBtn, showAllBtn, showTodayBtn, showUpcomingBtn, showCategoryBtn);
+
+    return div;
 }
 
 function main() {
