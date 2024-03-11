a8ed57b671dd667cff58105e653ec27d045ad52c
Ed Benedict Quia <edbenedictquia@gmail.com>
2024-03-11 20:31:34 +0800

    add todolist object

--- src/modules/logic.js
+++ src/modules/logic.js
@@ -12,3 +12,22 @@ function Project(name) {
     this.name = name;
     this.toDos = [];
 }
+
+// TODOLIST OBJECT
+const ToDoList = {
+    projects: [],
+    addProject: function(project) {
+        this.projects.push(project);
+    },
+    addToDoToProject: function(projectIndex, toDo) {
+        if (projectIndex >= 0 && projectIndex < this.projects.length) {
+            this.projects[projectIndex].addToDo(toDo);
+        } else {
+            console.error("Invalid project index");
+        }
+    },
+    addIndividualToDo: function(toDo) {
+        this.projects.push(new Project("Individual"));
+        this.addToDoToProject(this.projects.length - 1, toDo);
+    }
+};
